fdef2a0996a35dc4a732ae317bbac17d0e892058
Sarvesh <10650sarvesh@gmail.com>
2026-05-26 20:51:45 +0530

    get couriers

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -8,6 +8,7 @@ import {
   deleteDishController,
   deleteFlashDealController,
   deleteOfferController,
+  getCouriersController,
   getFlashDealsController,
   getOrdersController,
   raiseTicketController,
@@ -40,12 +41,12 @@ router.get("/get-offers", getOffersController);
 router.post("/add-offer", validate(OfferSchema), addOfferController);
 router.delete("/delete-offer/:offerId", deleteOfferController);
 
+router.get("/get-couriers", getCouriersController);
 router.post("/add-courier", validate(CourierSchema), addCourierController);
 router.put("/update-courier/:courierId", updateCourierController);
 router.delete("/delete-courier/:courierId", deleteCourierController);
 
-
-router.get("/get-flashDeals",getFlashDealsController)
+router.get("/get-flashDeals", getFlashDealsController);
 router.post(
   "/add-flashDeal",
   validate(FlashDealSchema),
@@ -58,7 +59,7 @@ router.put(
 );
 router.delete("/delete-flashDeal/:flashDealId", deleteFlashDealController);
 
-router.get("/get-orders",getOrdersController)
+router.get("/get-orders", getOrdersController);
 router.put("/update-order/:orderId", updateOrderController);
 
 router.post("/raise-ticket", validate(TicketSchema), raiseTicketController);
