6a0cae4d034f300512f45dff66ea37dcac1de496
Sarvesh <10650sarvesh@gmail.com>
2026-05-27 13:47:45 +0530

    logout bug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import adminRoutes from "./admin.routes";
 import userRoutes from "./user.routes";
 import registerRoutes from "./register.routes";
 import loginRoutes from "./login.routes";
+import logoutRouts from "./logout.routes";
 
 const router = Router();
 
